09e71c6d2f5ca58c908132b6534ac2975a959186
Estudiante <Estudiante@T203-21-2022>
2023-02-18 09:57:13 -0600

    Agregar archivos de proyecto.

---
+++ L1_RAMS_1018822/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1_RAMS_1018822
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("soy RUDY");
+            Console.ReadKey();
+
+            /*COMENTARIOS*/
+
+            Console.WriteLine("Hola Mundo soy RUDY");
+            Console.ReadKey();  
+
+            Console.WriteLine("Ingrese su nombre: ");
+            string Nombre = Console.ReadLine();
+            
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("soy " + Nombre);
+
+            /*COMENTARIOS*/
+
+            Console.Write("Hola Mundo ");
+            Console.Write("soy " + Nombre);
+            Console.ReadLine();
+
+
+            Console.WriteLine("");
+
+            Console.Write("Nombre: ");
+            string sNombre = Console.ReadLine();;
+
+            Console.Write("Edad: ");
+            string sEdad = Console.ReadLine();
+
+            Console.Write("Carrera: ");
+            string sCarrera = Console.ReadLine();
+
+            Console.Write("Carne: ");
+            string sCarne = Console.ReadLine();
+
+            Console.Write("Soy " + sNombre + ", tengo " + sEdad + " años y estudio la carrera de " + sCarrera + ". Mi nùmero de carnè es; " + sCarne);
+            Console.ReadLine();
+        }
+    }
+}
